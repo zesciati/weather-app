ab499eee32c469f4dedcf4f16fb04563060fa9f9
zesciati <zakyabiyyu2003@gmail.com>
2026-09-13 20:07:26 +0700

    menambahkan grid grid feels like

--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -2,14 +2,39 @@
 
 <template>
   <section
-    class="bg-[url(/images/bg-today-small.svg)] bg-center bg-no-repeat bg-size-[100%_100vh] flex items-center flex-col p-4"
+    class="bg-[url(/images/bg-today-small.svg)] bg-center bg-no-repeat bg-size-[100%_100vh] flex items-center flex-col p-11"
   >
-    <h1 class="text-3xl text-white">Berlin, Germany</h1>
-    <h3 class="text-white">Tuesday, Aug 5, 2025</h3>
-    <!-- <div class="flex ">
-      <img src="/images/icon-sunny.webp" alt="icon-sunny" class="w-20">
-      <h1 class="text-white">20°</h1>
-    </div> -->
-    
+    <div class="text-center space-y-2">
+      <h1 class="text-3xl text-white font-semibold">Berlin, Germany</h1>
+      <h3 class="text-white text-lg">Tuesday, Aug 5, 2025</h3>
+    </div>
+
+    <div class="flex items-center gap-4">
+      <img src="/images/icon-sunny.webp" alt="icon-sunny" class="w-30" />
+      <h1 class="text-white text-[5rem] font-semibold italic font-dmsans">
+        20°
+      </h1>
+    </div>
+  </section>
+
+  <section class="grid grid-cols-[repeat(auto-fit,minmax(120px,1fr))] gap-3 ">
+    <div class="text-white bg-neutral-700 p-3 space-y-4 rounded-lg border-2 border-neutral-800">
+      <p class="text-lg">Feels Like</p>
+      <span class="text-3xl">18°</span>
+    </div>
+
+    <div class="text-white bg-neutral-700 p-3 space-y-4 rounded-lg border-2 border-neutral-800">
+      <p class="text-lg">Humidity</p>
+      <span class="text-3xl">46%</span>
+    </div>
+
+    <div class="text-white bg-neutral-700 p-3 space-y-4 rounded-lg border-2 border-neutral-800">
+      <p class="text-lg">Wind</p>
+      <span class="text-3xl">14 km/h</span>
+    </div>
+    <div class="text-white bg-neutral-700 p-3 space-y-4 rounded-lg border-2 border-neutral-800">
+      <p class="text-lg">Precipitation</p>
+      <span class="text-3xl">0 mm</span>
+    </div>
   </section>
 </template>
--- src/template/TemplateWeatherApp.vue
+++ src/template/TemplateWeatherApp.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="p-4 bg-neutral-900">
+  <div class="p-4 bg-neutral-900 space-y-6">
     <slot/>
   </div>
 </template>
