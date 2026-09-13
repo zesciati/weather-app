b03f7cec5c837b6de21f20364a2e014edb159a5b
zesciati <zakyabiyyu2003@gmail.com>
2026-09-13 10:42:33 +0700

    fixing bg-today-small size

--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -1,22 +1,15 @@
 <script setup lang="ts"></script>
 
-
 <template>
-  <section class="bg-[url(/images/bg-today-small.svg)] h-auto   bg-no-repeat max-inline-full ">
-    <p>halo</p>
-    <p>halo</p>
-    <p>halo</p>
-    <p>halo</p>
-    <p>halo</p>
-    <p>halo</p>
-    <p>halo</p>
-    <p>halo</p>
-    <p>halo</p>
-    <p>halo</p>
-    <p>halo</p>
-    <p>halo</p>
-    <p>halo</p>
-    <p>halo</p>
+  <section
+    class="bg-[url(/images/bg-today-small.svg)] bg-center bg-no-repeat bg-size-[100%_100vh] flex items-center flex-col p-4"
+  >
+    <h1 class="text-3xl text-white">Berlin, Germany</h1>
+    <h3 class="text-white">Tuesday, Aug 5, 2025</h3>
+    <!-- <div class="flex ">
+      <img src="/images/icon-sunny.webp" alt="icon-sunny" class="w-20">
+      <h1 class="text-white">20°</h1>
+    </div> -->
+    
   </section>
-
 </template>
