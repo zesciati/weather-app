3dd66c6fec7becf945acacf10751d32053911c76
zesciati <zakyabiyyu2003@gmail.com>
2026-09-20 13:20:45 +0700

    Finishing component Switch to Imperial on both mobile & desktop

--- src/components/littlecomponents/SwitchToImperials.vue
+++ src/components/littlecomponents/SwitchToImperials.vue
@@ -2,24 +2,25 @@
 
 <template>
   <div class="text-neutral-0 flex justify-center items-center min-h-screen">
-    <div class="bg-neutral-800 p-2 rounded-lg w-50">
-      <button class="font-semibold">Switch to Imperial</button>
+    <div class="bg-neutral-800 p-2 rounded-lg w-50 font-dmsans">
+      <button class="font-semibold px-1.5 py-0.5 cursor-pointer  text-left w-full hover:outline hover:outline-neutral-0 hover:rounded-md">Switch to Imperial</button>
       <div class="divide-y divide-neutral-500">
-        <div class="flex-col flex py-2 space-y-1">
-          <span class="flex justify-between text-neutral-300 text-sm ">Temperature </span>
-          <span class="flex justify-between hover:bg-neutral-700  p-1.5">Celcius (°C) <span>✓</span></span>
-          <span class="flex justify-between p-1.5">Fahrenheit (°F) <span>✓</span></span>
+        <div class="flex-col flex py-2 ">
+          <span class="flex justify-between text-neutral-300 text-xs p-1.5 sm:text-sm">Temperature </span>
+          <span class="flex justify-between hover:bg-neutral-700  p-1.5 rounded-md text-sm sm:text-base">Celcius (°C) <span>✓</span></span>
+          <span class="flex justify-between hover:bg-neutral-700 p-1.5 rounded-md text-sm sm:text-base">Fahrenheit (°F) <span>✓</span></span>
         </div>
-        <div class="flex-col flex py-2 space-y-2">
-          <span class="flex justify-between text-neutral-300 text-sm">Wind Speed </span>
-          <span class="flex justify-between">km/h <span>✓</span></span>
-          <span class="flex justify-between">mph  <span>✓</span></span>
+        <div class="flex-col flex py-2 ">
+          <span class="flex justify-between text-neutral-300 text-xs p-1.5 sm:text-sm">Wind Speed </span>
+          <span class="flex justify-between hover:bg-neutral-700 p-1.5 rounded-md text-sm sm:text-base">km/h <span>✓</span></span>
+          <span class="flex justify-between hover:bg-neutral-700 p-1.5 rounded-md text-sm sm:text-base">mph  <span>✓</span></span>
         </div>
-        <!-- <div class="flex-col flex py-2 space-y-2">
-          <span class="flex justify-between text-neutral-300 text-sm">Precipitation </span>
-          <span class="flex justify-between">Milimeters (mm) <span>✓</span></span>
-          <span class="flex justify-between">Inches (in) <span>✓</span></span>
-        </div> -->
+
+        <div class="flex-col flex py-2 ">
+          <span class="flex justify-between text-neutral-300 text-xs p-1.5 sm:text-sm">Precipitation</span>
+          <span class="flex justify-between hover:bg-neutral-700 p-1.5 rounded-md text-sm sm:text-base">Milimeters(mm) <span>✓</span></span>
+          <span class="flex justify-between hover:bg-neutral-700 p-1.5 rounded-md text-sm sm:text-base">Inches (in)  <span>✓</span></span>
+        </div>
       </div>
     </div>
   </div>
